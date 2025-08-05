8e436d8fb1111908ebec0086d1533630b5fa31c3
AbdirayimovS <abdirayimovsardor20@gmail.com>
2025-08-06 00:30:53 +0900

    conv2d output shape mismatch with origal paper

--- yolo.py
+++ yolo.py
@@ -37,14 +37,16 @@ class YOLO(nn.Module):
     nn.Conv2d(512, 512, 1),
     nn.Conv2d(512, 1024, 3),
     nn.MaxPool2d(2, stride=2),
-   
+    # 1 x 1024 x 7 x 7
+    nn.Conv2d(1024, 512, 1), # 1 x 512, 7, 7
+    
+    nn.Conv2d(512, 1024, 3),  # 1, 1024, 5, 5
+    
     nn.Conv2d(1024, 512, 1),
-    nn.Conv2d(512, 1024, 3),
+    nn.Conv2d(512, 1024, 3),  # 1, 1024, 3, 3
 
-    nn.Conv2d(1024, 512, 1),
-    nn.Conv2d(512, 1024, 3),
-    nn.Conv2d(1024, 1024, 3),
-    )
+    nn.Conv2d(1024, 1024, 3),  # 1, 1024, 1, 1
+    # We have issue of mismatch between the shapes of paper and default torch conv2d operations!
     '''
     nn.Conv2d(1024, 1024, 3, stride=2),
     nn.Conv2d(1024, 1024, 3),
