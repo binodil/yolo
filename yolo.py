2da3f4c13ecd9bedd0ec12e6f94fdd1cd922b897
Sardor from WSL@Windows <abdirayimovsardor20@gmail.com>
2025-08-07 13:10:47 +0900

    Add ReLU in the end of the model. Model returns negative values so loss functions breaks into nan values

--- yolo.py
+++ yolo.py
@@ -19,9 +19,14 @@ class YOLO(nn.Module):
     nn.Conv2d(3, 64, 7, stride=2, padding=1),
     nn.MaxPool2d(2, stride=2, padding=1),  # expected size is 64x112x112
     
-    
+    nn.LeakyReLU(),
+    nn.BatchNorm2d(64), # hope to fix numerical instability
+
     nn.Conv2d(64, 192, 3, padding=1),
     nn.MaxPool2d(2, stride=2),  #192x56x56
+
+    nn.LeakyReLU(),
+    nn.BatchNorm2d(192), # hope to fix numerical instability
 
     nn.Conv2d(192, 128, 1, padding=1),
     nn.Conv2d(128, 256, 3, padding=1),
@@ -29,6 +34,8 @@ class YOLO(nn.Module):
     nn.Conv2d(256, 512, 3),
     nn.MaxPool2d(2, stride=2),  # 512x28x28
     
+    nn.LeakyReLU(),
+    nn.BatchNorm2d(512), # hope to fix numerical instability
 
     nn.Conv2d(512, 256, 1),
     nn.Conv2d(256, 512, 3, padding=1),
@@ -46,6 +53,8 @@ class YOLO(nn.Module):
     nn.Conv2d(512, 1024, 3, padding=1),
     nn.MaxPool2d(2, stride=2),  # 1024x14x14
 
+    nn.LeakyReLU(),
+    nn.BatchNorm2d(1024), # hope to fix numerical instability
 
     nn.Conv2d(1024, 512, 1),
     nn.Conv2d(512, 1024, 3, padding=1),
@@ -59,10 +68,11 @@ class YOLO(nn.Module):
 
     nn.Flatten(),
     nn.Linear(1024*S*S, 4096),
-    nn.ReLU(),
-    # Dropout?
+    nn.LeakyReLU(),
+    nn.Dropout(0.5),  # accor. to paper
     # LeakyReLU? BatchNorm?
     nn.Linear(4096, S*S*(C+5*B)),
+    nn.ReLU(),
     )
   def forward(self, x): return self.backbone(x)
 
@@ -71,9 +81,7 @@ if __name__ == "__main__":
   model = YOLO(S=7, B=2, C=20)  # same as in paper
   x = torch.randn(1, 3, 448, 448)
   out = model(x)
-  print(out.shape)
-  print(out.reshape(1, 7, 7, 30).shape)
-
-
-
+  print("Sum of nan values:", out.nansum())
+  print("Sum of nan manually: ", out.isnan().sum())
+  print("Is All values are positive?: ", out.abs().sum() == out.sum())
 
